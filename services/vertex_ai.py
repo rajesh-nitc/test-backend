a99d34bb24ac320581f146d7debadc4b1d4365ec
rajesh <rajesh.nitc@gmail.com>
2024-11-16 00:14:44 +0530

    chore: chore

--- services/vertex_ai.py
+++ services/vertex_ai.py
@@ -16,11 +16,8 @@ def generate_model_response(prompt: str, model: GenerativeModel) -> str:
     function_calls = extract_function_calls(response)
 
     if function_calls:
-        # Iterate through the list of function calls
-        for item in function_calls:
-            for k, v in item.items():
-                if k == "get_spend_func":
-                    return v
+        for k,v in function_calls[0].items():
+            return v
 
     # If no function calls, return the extracted text from the response
     return extract_text(response)
